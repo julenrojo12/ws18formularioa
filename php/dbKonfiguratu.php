c9ac25951f9aff6a4f16fa52755f9f8c2f2fda91
Angela Escondrillas <angelaescon@gmail.com>
2018-10-18 13:22:56 +0200

    Proba

---
+++ php/dbKonfiguratu.php
@@ -0,0 +1,15 @@
+<?php
+	$local = 1; //1 hodeian, lokalean 0
+	
+	if($local){
+		$zerbitzaria = "localhost";
+		$erabiltzailea = "id7132366_angelaescon"; //bakoitza berea
+		$gakoa = "qwertyuiop";
+		$db = "id7132366_quiz";
+	}else{
+		$zerbitzaria = "localhost";
+		$erabiltzailea = "root";
+		$gakoa = "";
+		$db = "quiz";
+	}
+?>
