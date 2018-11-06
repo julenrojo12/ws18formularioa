8e3d1aa0ad182f7374bab9b23540050a54509f39
wsjdae <43269881+wsjdae@users.noreply.github.com>
2018-11-06 19:33:25 +0100

    lab04 derr

---
+++ php/showXMLQuestions.php
@@ -0,0 +1,37 @@
+<style>	
+	table, th, td {
+		border: 1px solid black;
+		font-family: Trebuchet MS;
+	}
+	
+	th {
+		background-color: #834DFF;
+		color: white;
+	}
+</style>
+
+<?php
+	$file='../xml/questions.xml';
+	$xml= simplexml_load_file($file);
+	
+?>
+<table>
+	<thead>
+		<tr>
+			<th>Egilea</th>
+			<th>Enuntziatua</th>
+			<th>Erantzun zuzena</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+	foreach($xml->children() as $assessmentItem){
+		echo "<tr>";
+			echo "<td>" .$assessmentItem->attributes()->author."</td>";
+			echo "<td>" .$assessmentItem->itemBody->p."</td>";
+			echo "<td>" .$assessmentItem->correctResponse->value."</td>";
+		echo "</tr>";
+	}
+	?>
+	</tbody>
+</table>
